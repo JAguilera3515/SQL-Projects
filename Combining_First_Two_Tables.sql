3e2c69028213e5bb24fb32b3034eb036ac001f4e
Jose Aguilera <60164793+JAguilera3515@users.noreply.github.com>
2021-09-22 17:18:35 -0400

    Update Combining_First_Two_Tables.sql

--- Combining_First_Two_Tables.sql
+++ Combining_First_Two_Tables.sql
@@ -1,14 +1,16 @@
+CREATE TABLE feb_march AS
+SELECT *
+FROM (
 SELECT f1.`Account`,f1.`SubAccountNumber`,f1.`Service ID`,f1.`Component1`,f1.`Component2`,f1.feb_charges,m1.mar_charges
 FROM (
     SELECT `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,SUM(`Sum of CurrentMonthCharges`) feb_charges 
     FROM feb_2021 
-    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,`Sum of CurrentMonthCharges`) f1
+    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`) f1
 LEFT JOIN (
-    SELECT `Account`,`Service ID`,`Component1`,`Component2`,SUM(`Sum of CurrentMonthCharges`) mar_charges 
+    SELECT `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,SUM(`Sum of CurrentMonthCharges`) mar_charges 
     FROM mar_2021 
-    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`
-	) m1 
-ON f1.`Account`=m1.`Account` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`
+    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`) m1 
+ON f1.`Account`=m1.`Account` AND f1.`SubAccountNumber`=m1.`SubAccountNumber` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`
 
 UNION
 
@@ -16,10 +18,9 @@ SELECT m1.`Account`,m1.`SubAccountNumber`,m1.`Service ID`,m1.`Component1`,m1.`Co
 FROM (
     SELECT `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,SUM(`Sum of CurrentMonthCharges`) feb_charges 
     FROM feb_2021 
-    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,`Sum of CurrentMonthCharges`) f1
+    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`) f1
 RIGHT JOIN (
     SELECT `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`,SUM(`Sum of CurrentMonthCharges`) mar_charges 
     FROM mar_2021 
-    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`
-	) m1 
-ON f1.`Account`=m1.`Account` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`
+    GROUP BY `Account`,`SubAccountNumber`,`Service ID`,`Component1`,`Component2`) m1 
+ON f1.`Account`=m1.`Account` AND f1.`SubAccountNumber`=m1.`SubAccountNumber` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`) t
