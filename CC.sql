e90c9b640490480d3e4b628abba41276ed549145
Jose Aguilera <60164793+JAguilera3515@users.noreply.github.com>
2021-10-12 16:18:26 -0400

    Create CC.sql

---
+++ CC.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `jun10-aug20`
+SELECT f1.`Account`,f1.`Service ID`,f1.`Component1`,f1.`Component2`,f1.jun20_charge,f1.jul20_charge,m1.aug20_charge
+FROM `jun20-july20` f1
+LEFT JOIN 
+(
+	SELECT `Account`,`Service ID`,`Component1`,`Component2`, SUM(`Sum of CurrentMonthCharges`) aug20_charge FROM lumen_mrc_082020 f1
+    Group By `Account`,`Service ID`,`Component1`,`Component2`
+)m1 ON f1.`Account`=m1.`Account` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`
+
+UNION
+
+SELECT m1.`Account`,m1.`Service ID`,m1.`Component1`,m1.`Component2`,m1.jun20_charge,m1.jul20_charge,f1.aug20_charge
+FROM (
+    SELECT `Account`,`Service ID`,`Component1`,`Component2`, SUM(`Sum of CurrentMonthCharges`) aug20_charge FROM lumen_mrc_082020 f1
+    Group By `Account`,`Service ID`,`Component1`,`Component2`
+	) f1
+RIGHT JOIN `jun20-july20` m1 ON f1.`Account`=m1.`Account` AND f1.`Service ID`=m1.`Service ID` AND f1.`Component1`=m1.`Component1` AND f1.`Component2`=m1.`Component2`
